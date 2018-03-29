7db5193e12eb73499a2c90e7d0a30121dc4682fc
Ryan Cybul <ryan.cybul@gmail.com>
2018-03-28 23:12:40 -0500

    scripts

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,27 @@
-$(document).ready.function({
+$(document).ready(function() {
 
 //Google Maps Api Info
+async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDoeXJw_A8kRRjm5VsqzpWvLvqC2vGtXB0&callback=initMap"
 
 //National Park API Info
+
+//Variables
+
+
+//Functions
+function initMap() {
+    var uluru = {lat: -25.363, lng: 131.044};
+    var map = new google.maps.Map(document.getElementById('.js-container'), {
+      zoom: 4,
+      center: uluru
+    });
+    var marker = new google.maps.Marker({
+      position: uluru,
+      map: map
+    });
+  }
+
+//Events
+
 
 });
