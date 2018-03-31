dc1b31e353cf9d6705c4c209d5977458c24f796f
Ryan Cybul <ryan.cybul@gmail.com>
2018-03-31 18:25:00 -0500

    maddyUpdate

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+//Functions
 //Loads map on page load
 function initMap() {
     //Map options
@@ -5,12 +6,11 @@ function initMap() {
         zoom: 4.3,
         center:{lat:39.8283,lng:-98.5795}
     }
+    //Displays the map
+    let map = new
+    google.maps.Map(document.getElementById('map'), options);
 
-//Displays the map
-let map = new
-google.maps.Map(document.getElementById('map'), options);
-
-//AJAX Call for NPS API
+    //AJAX Call for NPS API
     let queryURL = "https://developer.nps.gov/api/v1/parks?limit=504&q=&api_key=1w64xYKjzt6YExOPqZE6qtvVHCE3ZAOO7xrQgUAV";
     console.log(queryURL);
     $.ajax({ url: queryURL, method: "GET" })
@@ -25,21 +25,26 @@ google.maps.Map(document.getElementById('map'), options);
 
     //Function to run markers
     function markers(parkDataArray){
-
-    //Loop through markers
     for(var i = 0; i < parkDataArray.length;i++){
-        let latLong = parkDataArray[i].latLong.split(', ');
-        let lat = +latLong[0].slice(4);
-        let long = +latLong[1].slice(5);
+        if(parkDataArray[i].latLong){
+            let latLong = parkDataArray[i].latLong.split(', ');
+            let lat = +latLong[0].slice(4);
+            let long = +latLong[1].slice(5);
 
-        let parkObj = {
-            name: parkDataArray[i].fullName,
-            coords:{lat: lat,lng: long}
+            let parkObj = {
+                name: parkDataArray[i].fullName,
+                description: parkDataArray[i].description,
+                coords:{lat: lat,lng: long},
+                directions: parkDataArray[i].directionsInfo,
+                weather: parkDataArray[i].weatherInfo,
+                url: parkDataArray[i].url,
+            }
+            console.log(parkObj);
+            addMarker(parkObj);
+            }
         }
-        console.log(parkObj);
-        addMarker(parkObj);
+
     }
-}
 
     //Add marker function
     function addMarker(parkInfo) {
@@ -48,12 +53,15 @@ google.maps.Map(document.getElementById('map'), options);
             position: parkInfo.coords,
             map: map,
             name: parkInfo.name,
+            description: parkInfo.description,
+            directions: parkInfo.directions,
             icon: './assets/images/Tree_Icon.svg'
         });
 
         //Info window click event
+        let infoWindowContent = "<div >" + parkInfo.name + "</div>" + "<div>" + parkInfo.description + "</div>" + '<button>'+'More Info'+'</button>';
         let infoWindow = new google.maps.InfoWindow({
-        content: parkInfo.name
+        content: infoWindowContent,
         });
         
         //To do: Make it so only one info window can be open at once. 
@@ -61,6 +69,7 @@ google.maps.Map(document.getElementById('map'), options);
         marker.addListener('click', function(){
         infoWindow.open(map, marker);
         });
+        
         
     }
 }
