192f73b86b7264795ffe961d1241beec888f57b0
Ryan Cybul <ryan.cybul@gmail.com>
2018-03-30 20:30:08 -0500

    mapUpdate

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,26 @@
 $(document).ready(function() {
 
 //Google Maps Api Info
-async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDoeXJw_A8kRRjm5VsqzpWvLvqC2vGtXB0&callback=initMap"
 
 //National Park API Info
 
 //Variables
 
-
 //Functions
 function initMap() {
-    var uluru = {lat: -25.363, lng: 131.044};
-    var map = new google.maps.Map(document.getElementById('.js-container'), {
-      zoom: 4,
-      center: uluru
-    });
-    var marker = new google.maps.Marker({
-      position: uluru,
-      map: map
-    });
-  }
+    let options = {
+        zoom: 8,
+        center:{lat:41.896798,lng:-87.618804}
+    }
+    let map = new
+    google.maps.Map(document.getElementById('map'), options);
+}
+
+
+//Sets the correct language for Google Maps and adds API script.
 
 //Events
+//initMap();
 
 
 });
