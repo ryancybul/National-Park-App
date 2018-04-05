41959f3c68d3a8bc8582fbd9a81bcbfd9fe154d3
Ryan Cybul <ryan.cybul@gmail.com>
2018-04-04 20:52:56 -0500

    combinedFiles

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,13 +2,12 @@
 function initMap() {
     //Map options
     let options = {
-        zoom: 4.6,
-        center:{lat:38.8283,lng:-98.5795}
-    }
+    zoom: 4.6,
+    center:{lat:38.8283,lng:-98.5795}
+}
     //Displays the map
     let map = new
-    google.maps.Map(document.getElementById('map'), options);
-
+    google.maps.Map(document.getElementById('map'), options)
     //AJAX Call for NPS API
     let queryURL = "https://developer.nps.gov/api/v1/parks?fields=images&limit=100&q=National+Park&api_key=1w64xYKjzt6YExOPqZE6qtvVHCE3ZAOO7xrQgUAV";
     console.log(queryURL);
@@ -24,7 +23,31 @@ function initMap() {
     //Hide page elements until park is clicked. 
     $(".js-info").hide();
     $(".js-pics").hide();
-}
+
+    var directionsService = new google.maps.DirectionsService;
+    var directionsDisplay = new google.maps.DirectionsRenderer({suppressMarkers: true});;
+    directionsDisplay.setMap(map);
+
+    var onChangeHandler = function() {
+      calculateAndDisplayRoute(directionsService, directionsDisplay);
+    };
+    document.getElementById('start').addEventListener('change', onChangeHandler);
+    document.getElementById('end').addEventListener('change', onChangeHandler);
+  }
+
+    function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+    directionsService.route({
+      origin: document.getElementById('start').value,
+      destination: document.getElementById('end').value,
+      travelMode: 'DRIVING'
+    }, function(response, status) {
+      if (status === 'OK') {
+        directionsDisplay.setDirections(response);
+      } else {
+        window.alert('Directions request failed due to ' + status);
+      }
+    });
+  }
 
 //Function to run markers
 function markers(parkDataArray, map){
@@ -40,15 +63,25 @@ function markers(parkDataArray, map){
                 coords:{lat: lat,lng: long},
                 directions: parkDataArray[i].directionsInfo,
                 weather: parkDataArray[i].weatherInfo,
+                lat: lat,
+                long: long,
                 url: parkDataArray[i].url,
                 states: parkDataArray[i].states,
                 images: parkDataArray[i].images
             }
             console.log(parkObj);
+            console.log(parkObj.name);
             addMarker(parkObj, map);
+            directions(parkObj);
             }
         }
     }
+
+function directions(parkObj){
+    console.log('Console log: '+parkObj.lat+', '+parkObj.long);
+    $('#start').append('<option value="'+parkObj.lat+', '+parkObj.long+'">'+parkObj.name+'</option>');
+    $('#end').append('<option value="'+parkObj.lat+', '+parkObj.long+'">'+parkObj.name+'</option>');
+}
     
 //Add marker function
 function addMarker(parkInfo, map) {
@@ -76,6 +109,8 @@ function addMarker(parkInfo, map) {
     //To do: Make it so only one info window can be open at once. 
     //When marker is clicked open window. 
     marker.addListener('click', function(){
+        //Centers to marker
+        map.panTo(marker.getPosition());    
         infoWindow.open(map, marker);
 
         //Hide page elements until park is clicked. 
@@ -105,7 +140,7 @@ function addMarker(parkInfo, map) {
             scrollTop: $(".js-info").offset().top
         }, 1000);
     })
-//
+
 
 
 
